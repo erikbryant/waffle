b626ca0a775957e7d6a2b102a9cb6bb99085ce73
Erik Bryant <erikbryantology@gmail.com>
2023-06-24 22:33:52 -0400

    Add CPU profiling

--- Makefile
+++ Makefile
@@ -10,5 +10,9 @@ test: vet
 run: test
 	cd main ; go run ./...
 
+pprof: test
+	cd main ; go run waffle.go -cpuprofile cpu.prof
+	echo "top10" | go tool pprof main/cpu.prof
+
 # Targets that do not represent actual files
-.PHONY: fmt vet test run
+.PHONY: fmt vet test run pprof
--- main/waffle.go
+++ main/waffle.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/erikbryant/waffle/board"
 	"github.com/erikbryant/waffle/pathfinder"
 	"github.com/erikbryant/waffle/solver"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 )
 
 func main() {
 	fmt.Println("Welcome to waffle!")
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	serial := "smkupm.w.nknbeui.e.rgaiey/gyywgw.w.ywygwyw.y.wgwyyg" // 515
 
