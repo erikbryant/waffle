f3162d3ba83578886dda1fa83ee5181cd488243b
Erik Bryant <erikbryantology@gmail.com>
2023-06-24 23:09:23 -0400

    Remove extraneous fluff

--- regress/regress.go
+++ regress/regress.go
@@ -15,29 +15,26 @@ var (
 )
 
 func TestSolve() {
-	testCases := []struct {
-		serial   string
-		solvable bool
-	}{
+	testCases := []string{
 		// Deluxe Waffles
-		{"eifdstal.i.p.apertislt.e.e.senithvte.m.t.ueuedrra/yygygwyw.w.w.wgwgggwgw.w.y.wgwgggwgw.w.y.wywgygww", true}, // 056
+		"eifdstal.i.p.apertislt.e.e.senithvte.m.t.ueuedrra/yygygwyw.w.w.wgwgggwgw.w.y.wgwgggwgw.w.y.wywgygww", // 056
 
 		// Daily Waffles
-		{"fboueg.i.ulsoomg.e.loemna/gwwggw.w.wgygyyw.y.wgyywg", true}, // 001
-		{"scgoln.n.dindeer.i.uffare/gwwwgg.w.yyggwyw.y.wgwyyg", true}, // 002
-		{"mmkoye.u.iaomerr.o.pahcln/gwywgw.y.wyggwyw.w.wgwwwg", true}, // 100
-		{"lieirs.w.riipese.n.rcouye/gwywgw.y.wygggyw.w.wgwywg", true}, // 200
-		{"agdrml.r.ueianeu.l.oeibrr/gwywgw.y.wwygywg.w.ggwywg", true}, // 300
-		{"bsmcye.s.eaiarsl.l.adeeks/gwywgy.y.yywgwyw.y.wgwgwg", true}, // 400
-		{"daieoe.e.couvowr.g.glnene/gwywgy.g.yywgwyw.w.wgwywg", true}, // 500
-		{"eqebla.m.eupirel.n.mdlwal/ggywgw.w.ywygwww.g.wgyywg", true}, // 509
-		{"tuaehl.r.emrdcnu.i.heoeby/gwgygw.w.wyygwww.g.wgywyg", true}, // 513
-		{"bexkrd.c.aemarih.k.geasat/gywygy.w.ywygyww.g.wgwywg", true}, // 514
-		{"smkupm.w.nknbeui.e.rgaiey/gyywgw.w.ywygwyw.y.wgwyyg", true}, // 515
+		"fboueg.i.ulsoomg.e.loemna/gwwggw.w.wgygyyw.y.wgyywg", // 001
+		"scgoln.n.dindeer.i.uffare/gwwwgg.w.yyggwyw.y.wgwyyg", // 002
+		"mmkoye.u.iaomerr.o.pahcln/gwywgw.y.wyggwyw.w.wgwwwg", // 100
+		"lieirs.w.riipese.n.rcouye/gwywgw.y.wygggyw.w.wgwywg", // 200
+		"agdrml.r.ueianeu.l.oeibrr/gwywgw.y.wwygywg.w.ggwywg", // 300
+		"bsmcye.s.eaiarsl.l.adeeks/gwywgy.y.yywgwyw.y.wgwgwg", // 400
+		"daieoe.e.couvowr.g.glnene/gwywgy.g.yywgwyw.w.wgwywg", // 500
+		"eqebla.m.eupirel.n.mdlwal/ggywgw.w.ywygwww.g.wgyywg", // 509
+		"tuaehl.r.emrdcnu.i.heoeby/gwgygw.w.wyygwww.g.wgywyg", // 513
+		"bexkrd.c.aemarih.k.geasat/gywygy.w.ywygyww.g.wgwywg", // 514
+		"smkupm.w.nknbeui.e.rgaiey/gyywgw.w.ywygwyw.y.wgwyyg", // 515
 	}
 
 	for _, testCase := range testCases {
-		waffle := board.Parse(testCase.serial)
+		waffle := board.Parse(testCase)
 		s := solver.New(waffle)
 		s.Solve()
 	}
