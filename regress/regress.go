dd337fccb95c35215940108e5ca1443a7289c4ec
Erik Bryant <erikbryantology@gmail.com>
2023-06-24 23:24:45 -0400

    Add more regression tests

--- regress/regress.go
+++ regress/regress.go
@@ -22,15 +22,25 @@ func TestSolve() {
 		// Daily Waffles
 		"fboueg.i.ulsoomg.e.loemna/gwwggw.w.wgygyyw.y.wgyywg", // 001
 		"scgoln.n.dindeer.i.uffare/gwwwgg.w.yyggwyw.y.wgwyyg", // 002
+		"speeda.t.ptocirn.e.mpeiey/gggwgw.w.yywgwyw.g.wgyywg", // 003
+		"ndeeye.e.ltraeck.a.idnsks/gygygy.y.wwwgwyy.w.ygwywg", // 004
+		"crmvpe.r.glaivye.n.belouy/ggwwgw.w.yywgwyw.w.wgwywg", // 005
 		"mmkoye.u.iaomerr.o.pahcln/gwywgw.y.wyggwyw.w.wgwwwg", // 100
 		"lieirs.w.riipese.n.rcouye/gwywgw.y.wygggyw.w.wgwywg", // 200
 		"agdrml.r.ueianeu.l.oeibrr/gwywgw.y.wwygywg.w.ggwywg", // 300
 		"bsmcye.s.eaiarsl.l.adeeks/gwywgy.y.yywgwyw.y.wgwgwg", // 400
 		"daieoe.e.couvowr.g.glnene/gwywgy.g.yywgwyw.w.wgwywg", // 500
 		"eqebla.m.eupirel.n.mdlwal/ggywgw.w.ywygwww.g.wgyywg", // 509
+		"bnueet.p.moeioaz.o.sluwer/gwywgw.g.wywgwyw.y.wgywyg", // 510
+		"weoakm.e.hocberm.i.nprerl/gwwygw.y.wgwgyyw.w.ygyywg", // 511
+		"fiaclt.u.eatolhu.t.ltarae/gwywgw.y.wyygyyw.y.wgwywg", // 512
 		"tuaehl.r.emrdcnu.i.heoeby/gwgygw.w.wyygwww.g.wgywyg", // 513
 		"bexkrd.c.aemarih.k.geasat/gywygy.w.ywygyww.g.wgwywg", // 514
 		"smkupm.w.nknbeui.e.rgaiey/gyywgw.w.ywygwyw.y.wgwyyg", // 515
+		"cetrlu.i.haalcrt.n.epncih/gyyygw.y.wwwgwww.y.wgwywg", // 516
+		"feeonn.w.rewdinl.g.edolly/gwyygw.w.wgygwyw.w.ygwywg", // 517
+		"mulnsa.i.ilrboue.o.tfuily/gyyygw.w.wywgwyw.y.wgwywg", // 518
+		"puiweu.o.toeieeq.t.hrsaor/gyywgw.w.yywgwyw.y.wgwwgg", // 519
 	}
 
 	for _, testCase := range testCases {
