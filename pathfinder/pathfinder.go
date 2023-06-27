8c6ed7aa30ead139cddc0ea78e52b036bbb71229
Erik Bryant <erikbryantology@gmail.com>
2023-06-27 15:55:06 -0400

    Path finding works
    The code can now find a sequence of swaps that will solve the game. It
    is not necessarily the optimal solution. Making it optimal will have to
    come later.

--- pathfinder/pathfinder.go
+++ pathfinder/pathfinder.go
@@ -18,11 +18,10 @@ type Swap struct {
 
 // swappable contains the coordinates of a tile that needs swapping.
 type Swappable struct {
-	row     int
-	col     int
-	have    rune
-	want    rune
-	sortKey rune
+	row  int
+	col  int
+	have rune
+	want rune
 }
 
 type Path struct {
@@ -45,19 +44,76 @@ func (p *Path) Size() int {
 	return p.solution.Size()
 }
 
-// sort sorts swappable in the fewest swaps (selection sort)
-func sort(swappable []Swappable) ([]Swappable, []Swap) {
+// findLowest returns the index of the lowest key in the slice
+func findLowest(swappable []Swappable) int {
+	lowVal := swappable[0].want
+	lowIndex := 0
+
+	for i := range swappable {
+		if swappable[i].want < lowVal {
+			lowVal = swappable[i].want
+			lowIndex = i
+		}
+	}
+
+	return lowIndex
+}
+
+// sortWant sorts swappable by 'want'
+func sortWant(swappable []Swappable) []Swappable {
+	for i := 0; i < len(swappable)-1; i++ {
+		lowIndex := findLowest(swappable[i+1:]) + i + 1
+		if swappable[lowIndex].want < swappable[i].want {
+			// Swap!
+			swappable[i], swappable[lowIndex] = swappable[lowIndex], swappable[i]
+		}
+	}
+
+	return swappable
+}
+
+// findFirst finds the first letter that is swappable and matches 'want'
+func findFirst(want rune, swappable []Swappable) int {
+	for i, swap := range swappable {
+		if swap.have == swap.want {
+			// The letter is already on the right tile; don't swap it
+			fmt.Println("Skipping (1)", swap)
+			continue
+		}
+		if swap.have == want {
+			return i
+		}
+	}
+
+	// We failed to find a letter to swap; fatal error!
+	return -999
+}
+
+// sortHave sorts swappable by 'have' in the fewest swaps
+func sortHave(swappable []Swappable) ([]Swappable, []Swap) {
 	swaps := []Swap{}
 
-	// TODO: implement sort
+	for i := 0; i < len(swappable)-1; i++ {
+		if swappable[i].have == swappable[i].want {
+			// The letter is already on the right tile; don't swap it
+			continue
+		}
+		index := findFirst(swappable[i].want, swappable[i+1:]) + i + 1
+
+		// Record the swap
+		s1 := swappable[index]
+		s2 := swappable[i]
+		swaps = append(swaps, Swap{s1.have, s1.row, s1.col, s2.have, s2.row, s2.col})
+
+		// Swap!
+		swappable[i].have, swappable[index].have = swappable[index].have, swappable[i].have
+	}
 
 	return swappable, swaps
 }
 
 // Find finds a shortest path for swapping tiles to get to the solution
 func (p *Path) Find() {
-	fmt.Println("Finding shortest path...")
-
 	// Collect all tiles that need to be swapped and what letter they want to be
 	swappable := []Swappable{}
 	for _, tile := range p.solution.Tiles() {
@@ -65,27 +121,21 @@ func (p *Path) Find() {
 			continue
 		}
 		want := p.solution.GetSolution(tile.Row, tile.Col)
-		swappable = append(swappable, Swappable{tile.Row, tile.Col, tile.Letter, want, 'X'})
+		swappable = append(swappable, Swappable{tile.Row, tile.Col, tile.Letter, want})
 	}
 
 	// Sort 'swappable' by 'swappable.want'
-	for i := range swappable {
-		swappable[i].sortKey = swappable[i].want
-	}
-	swappable, _ = sort(swappable)
+	swappable = sortWant(swappable)
 
 	// Sort 'swappable' by 'swappable.have', recording each swap
-	for i := range swappable {
-		swappable[i].sortKey = swappable[i].have
-	}
-	_, p.swaps = sort(swappable)
+	swappable, p.swaps = sortHave(swappable)
 }
 
 // Print prints a representation of the solver state and shortest path to the console
 func (p *Path) Print() {
 	p.solution.Print()
 	fmt.Println()
-	fmt.Printf("A shortest set of swaps is %d swaps:\n", len(p.swaps))
+	fmt.Printf("A solution in %d swaps:\n", len(p.swaps))
 	for _, swap := range p.swaps {
 		fmt.Printf("  %c @ (%d, %d) <-> %c @ (%d, %d)\n", swap.l1, swap.r1, swap.c1, swap.l2, swap.r2, swap.c2)
 	}
