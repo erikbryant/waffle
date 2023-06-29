c2d2ade91f2d572e546e559b2c659844ef59b5d5
Erik Bryant <erikbryantology@gmail.com>
2023-06-28 21:42:12 -0400

    Remove code for invalid board states

--- solver/solver_test.go
+++ solver/solver_test.go
@@ -7,26 +7,22 @@ import (
 
 func TestSolve(t *testing.T) {
 	testCases := []struct {
-		serial   string
-		solvable bool
+		serial string
 	}{
 		// Deluxe Waffles
-		{"eifdstal.i.p.apertislt.e.e.senithvte.m.t.ueuedrra/yygygwyw.w.w.wgwgggwgw.w.y.wgwgggwgw.w.y.wywgygww", true}, // 056
+		{"eifdstal.i.p.apertislt.e.e.senithvte.m.t.ueuedrra/yygygwyw.w.w.wgwgggwgw.w.y.wgwgggwgw.w.y.wywgygww"}, // 056
 
 		// Daily Waffles
-		{"mmkoye.u.iaomerr.o.pahcln/gwywgw.y.wyggwyw.w.wgwwwg", true}, // 100
-		{"daieoe.e.couvowr.g.glnene/gwywgy.g.yywgwyw.w.wgwywg", true}, // 500
-
-		// Unsolvable Waffles
-		// {"abcd.e.fghi/wwww.w.wwww", false},
+		{"mmkoye.u.iaomerr.o.pahcln/gwywgw.y.wyggwyw.w.wgwwwg"}, // 100
+		{"daieoe.e.couvowr.g.glnene/gwywgy.g.yywgwyw.w.wgwywg"}, // 500
 	}
 
 	for _, testCase := range testCases {
 		waffle := board.Parse(testCase.serial)
 		s := New(waffle)
 		solved := s.Solve()
-		if solved != testCase.solvable {
-			t.Errorf("ERROR: For %s expected %t got %t", testCase.serial, testCase.solvable, solved)
+		if !solved {
+			t.Errorf("ERROR: Failed to solve %s", testCase.serial)
 		}
 	}
 }
