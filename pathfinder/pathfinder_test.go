47504ad6379c79b79693711e6cd2024928803faa
Erik Bryant <erikbryantology@gmail.com>
2023-07-02 15:34:01 -0400

    Add tests

---
+++ pathfinder/pathfinder_test.go
@@ -0,0 +1,47 @@
+package pathfinder
+
+import (
+	"testing"
+)
+
+func TestFindDouble(t *testing.T) {
+	testCases := []struct {
+		swappable []Swappable
+		have      rune
+		want      rune
+		expected  int
+	}{
+		{[]Swappable{}, 'a', 'b', -999},
+		{[]Swappable{{1, 1, 'b', 'a'}}, 'a', 'b', 0},
+		{[]Swappable{{0, 1, 'b', 'c'}, {1, 1, 'b', 'a'}}, 'a', 'b', 1},
+		{[]Swappable{{0, 1, 'b', 'c'}, {0, 1, 'c', 'a'}}, 'a', 'b', -999},
+	}
+
+	for _, testCase := range testCases {
+		index := findDouble(testCase.want, testCase.have, testCase.swappable)
+		if index != testCase.expected {
+			t.Errorf("ERROR: For %v expected %d got %d", testCase.swappable, testCase.expected, index)
+		}
+	}
+}
+
+func TestFindFirst(t *testing.T) {
+	testCases := []struct {
+		swappable []Swappable
+		want      rune
+		expected  int
+	}{
+		{[]Swappable{}, 'b', -999},
+		{[]Swappable{{1, 1, 'b', 'a'}}, 'b', 0},
+		{[]Swappable{{0, 1, 'b', 'c'}, {1, 1, 'b', 'a'}}, 'b', 0},
+		{[]Swappable{{0, 1, 'd', 'c'}, {1, 1, 'b', 'a'}}, 'b', 1},
+		{[]Swappable{{0, 1, 'x', 'c'}, {0, 1, 'c', 'a'}}, 'b', -999},
+	}
+
+	for _, testCase := range testCases {
+		index := findFirst(testCase.want, testCase.swappable)
+		if index != testCase.expected {
+			t.Errorf("ERROR: For %v expected %d got %d", testCase.swappable, testCase.expected, index)
+		}
+	}
+}
