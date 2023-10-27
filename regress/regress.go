dda2e8ce7a0fa646f40d48c7f7831ca82ea69dd5
Erik Bryant <erikbryantology@gmail.com>
2023-10-27 18:41:09 -0400

    Update to latest modules

--- regress/regress.go
+++ regress/regress.go
@@ -90,6 +90,7 @@ var (
 		{"taenergt r e perpsersd m x iaretaler m n vscsrevd/ywgygyww y w wgygygwgy w w ygygwgwgy w w wwwgygwy", 1},
 	}
 	dailyWaffles = []TestCase{
+		{"lnnyge s ustaime s reavny/gyyygg w gywgwyw w wgwywg", 644},
 		{"awdetp e iebrhht t bcoiuy/gwyygw g wwygywy w ggwywg", 643},
 		{"psaohc r boebsar d feeiry/gwywgw y wyygyyw w wgwgwg", 642},
 		{"flrohs r durdnsl e etehoe/ggywgy w ywygwww g wgyywg", 641},
