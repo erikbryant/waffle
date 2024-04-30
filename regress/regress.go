aab76d74bfc67a95a108a1196e4c6d124e96e818
Erik Bryant <erikbryantology@gmail.com>
2024-04-29 21:20:11 -0400

    Add daily waffle

--- regress/regress.go
+++ regress/regress.go
@@ -119,6 +119,7 @@ var (
 		{"taenergt r e perpsersd m x iaretaler m n vscsrevd/ywgygyww y w wgygygwgy w w ygygwgwgy w w wwwgygwy", 1},
 	}
 	dailyWaffles = []TestCase{
+		{"fnoana w xroivoa r lkmned/gwgwgy w wyygwyw w wgwyyg", 831},
 		{"fxupse l sominer p ltdpot/gwywgw w wgwgwgy y ygwywg", 830},
 		{"flsihi o ecanero z oontge/gwwwgy y wwwgggy w ygyywg", 829},
 		{"mtstee a iatutno d togerr/gwwwgw g wwygywy y ygwywg", 828},
